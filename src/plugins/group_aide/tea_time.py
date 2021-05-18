1ef18d6e0661d3331a4a0c319eb2e72bc87e3dcb
desolution025 <1484961843@qq.com>
2021-05-19 00:16:04 +0800

    在服务器上测试一下

--- src/plugins/group_aide/tea_time.py
+++ src/plugins/group_aide/tea_time.py
@@ -38,7 +38,8 @@ scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 
 # @scheduler.scheduled_job("cron", day_of_week='0-6', hour=15)
-@scheduler.scheduled_job("cron", day_of_week='mon-sat', hour=15, id='3clock')
+# @scheduler.scheduled_job("cron", day_of_week='mon-sat', hour=15, id='3clock')
+@scheduler.scheduled_job("cron", day_of_week='mon-sat', hour=0, minute=20, id='3clock', misfire_grace_time=600)
 async def three_clock():
     logger.info('Time to drink tea!!')
     for strid, bot in get_bots().items():
@@ -50,11 +51,12 @@ async def three_clock():
         logger.info(f'Will Send {len(gids)} group(s): {str(gids)}')
         try:
             for g in gids:
-                for msg in TEATIME_NOTICE:
-                    await bot.send_group_msg(group_id=g, message=msg)
-                    await asyncio.sleep(1.5)  # 防止消息发送间隔过短导致顺序错乱
-                await bot.send_group_msg(group_id=g, message=choice(TEATIME_ATTACH))
-                asyncio.sleep(15)  # 结束上一个群的发送开始发送下一个群的间隔
+                if g == 935673561:  # 记得删掉这行啊
+                    for msg in TEATIME_NOTICE:
+                        await bot.send_group_msg(group_id=g, message=msg)
+                        await asyncio.sleep(1.5)  # 防止消息发送间隔过短导致顺序错乱
+                    await bot.send_group_msg(group_id=g, message=choice(TEATIME_ATTACH))
+                    asyncio.sleep(15)  # 结束上一个群的发送开始发送下一个群的间隔
         except ActionFailed as err:
             logger.error('Maybe too intensive to send msg, cause error: {err}')
 
